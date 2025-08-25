26e0895ee525d3010a0f120214604b82b073c6ee
brunohro <brunoh.rodrigues27@gmail.com>
2025-08-25 10:29:24 -0300

    Criando models para sensores

--- app_principal/admin.py
+++ app_principal/admin.py
@@ -1,1 +1,14 @@
 from django.contrib import admin
+from .models import TipoSensor, LeituraSensor
+
+@admin.register(TipoSensor)
+class TipoSensorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome')
+    search_fields = ('nome',)
+
+
+@admin.register(LeituraSensor)
+class LeituraSensorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tipo_sensor', 'valor_percentual', 'data_criacao')
+    list_filter = ('tipo_sensor', 'data_criacao')
+    search_fields = ('tipo_sensor__nome',)
--- app_principal/models.py
+++ app_principal/models.py
@@ -1,4 +1,34 @@
 from django.db import models
+from django.utils import timezone
 
+class TipoSensor(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+    descricao = models.TextField(blank=True, null=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.nome
+    
+
+class LeituraSensor(models.Model):
+    tipo_sensor = models.ForeignKey(TipoSensor, on_delete=models.CASCADE, related_name="leituras")
+    valor_percentual = models.FloatField(help_text="Valor em porcentagem ( 0 a 100 )")
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.tipo_sensor.nome} - {self.valor_percentual} ({self.data_criacao.strftime('%d/%m/%Y %H:%M')})"
+    
+    @classmethod
+    def dias_disponiveis(cls):
+        """Retorna os dias que possuem leituras registradas."""
+        return (
+            cls.objects
+            .dates('data_criacao', 'day', order='DESC')
+        )
+    
+    @classmethod
+    def leituras_por_dia(cls, dia_selecionado, tipo_sensor=None):
+        """Retorna leituras filtradas por dia e, opcionalmente, por tipo de sensor."""
+        consultas = cls.objects.filter(data_criacao__date=dia_selecionado)
+        if tipo_sensor:
+            consultas = consultas.filter(tipo_sensor=tipo_sensor)
+        return consultas.order_by('data_criacao')
